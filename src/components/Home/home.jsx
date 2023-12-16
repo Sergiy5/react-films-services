8e66ab6dfab3d57def258d7630786ba6083a2246
Sergiy Kushnir <s1983kushnir@gmail.com>
2023-12-16 18:46:58 +0200

    Fixed js.config

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -1,11 +1,12 @@
 import ListMovies from 'components/ListMovies/listMovies';
 import { useState, useEffect } from 'react';
-import {trendMovies} from "utils/searchDataMovie";
+import { trendMovies } from "utils/searchDataMovie";
+// import { mostPopularMovies } from 'utils/searchDataMovie';
+
 
 const Home = () => {
     const [movies, setMovies] = useState([])
-
- 
+  console.log(movies)
   useEffect(() => {
         trendMovies().then(data => setMovies(data.results));
     }, []);
--- src/utils/searchDataMovie.js
+++ src/utils/searchDataMovie.js
@@ -1,59 +1,69 @@
+// 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5OGU5MzBjMmZhNzRjYWVhNjRmMjAyNmNmNzBlYzQxMyIsInN1YiI6IjY0NmQwNWJhZDE4NTcyMDE4MDJlOGYyOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gvC078v2ZEDlsUKlhf6XJhVHnGo7gwYMbWV5S3NVSKY';
 
-
-  const API_KEY = 'bed58f4c3dbb6f0a7987e1858ff87c95';
-  const BASE_URL = 'https://api.themoviedb.org/3';
-  const TREND_URL = `${BASE_URL}/trending/all/day`;
-  const SEARCH_URL = `${BASE_URL}/search/movie`;
+// const API_KEY = '98e930c2fa74caea64f2026cf70ec413';
+const API_KEY = 'k_yjb6zjrs';
+const BASE_URL = 'https://api.themoviedb.org/3';
+// const BASE_URL = 'https://imdb-api.com/en/API';
+// const Most_Popular_Movies = `${BASE_URL}/MostPopularMovies/${API_KEY}`;
+const TREND_URL = `${BASE_URL}/trending/all/day`;
+const SEARCH_URL = `${BASE_URL}/search/movie`;
 const ID_URL = `${BASE_URL}/movie`;
-  
 
-  const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization:
-        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZWQ1OGY0YzNkYmI2ZjBhNzk4N2UxODU4ZmY4N2M5NSIsInN1YiI6IjY0NmQwNWJhZDE4NTcyMDE4MDJlOGYyOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4jCqRdZ9sRAbpi5bh6szcuiE2wlzfaZQtIR0L9NKky0',
-    },
-  };
 
-  export async function trendMovies(page = 1) {
-    return await fetch(TREND_URL, options)
-      .then(response => {
-        return response.json();
-      })
-      .catch(error => console.log(error));
-  }
-  
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization:
+      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5OGU5MzBjMmZhNzRjYWVhNjRmMjAyNmNmNzBlYzQxMyIsInN1YiI6IjY0NmQwNWJhZDE4NTcyMDE4MDJlOGYyOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gvC078v2ZEDlsUKlhf6XJhVHnGo7gwYMbWV5S3NVSKY',
+  },
+};
+
+export async function trendMovies(page = 1) {
+  return await fetch(TREND_URL, options)
+    .then(response => {
+      return response.json();
+    })
+    .catch(error => console.log(error));
+}
+
+// export async function mostPopularMovies(page = 1) {
+//   return await fetch(Most_Popular_Movies)
+//     .then(response => {
+//       console.log(response)
+//       return response.json();
+//     })
+//     .catch(error => console.log(error));
+// }
+
 export async function getMovieById(id) {
   try {
     const data = await fetch(
       `${ID_URL}/${id}?api_key=${API_KEY}&language=en-US`,
       options
-    )
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        };
-        throw new Error()
-      })
-    return data
+    ).then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error();
+    });
+    return data;
   } catch (error) {
     alert('Тут зовсім порожньо');
   }
 }
 
-
-export  async function FetchAPI(query, page=1) {
+export async function FetchAPI(query, page = 1) {
   try {
-   return await fetch(
-     `${SEARCH_URL}?api_key=${API_KEY}&language=en-US&query=${query}&page=${page}`,
-     options
-    ).then(res=> res.json());
- } catch (error) {
-   console.error('Smth wrong with api search fetch' + error);
- }
+    return await fetch(
+      `${SEARCH_URL}?api_key=${API_KEY}&language=en-US&query=${query}&page=${page}`,
+      options
+    ).then(res => res.json());
+  } catch (error) {
+    console.error('Smth wrong with api search fetch' + error);
+  }
 }
-    
+
 export async function getMovieCredits(id) {
   try {
     const data = await fetch(
@@ -70,7 +80,7 @@ export async function getMovieCredits(id) {
     alert('Тут зовсім порожньо');
   }
 }
- 
+
 export async function getMovieReviews(id) {
   try {
     const data = await fetch(
@@ -87,4 +97,4 @@ export async function getMovieReviews(id) {
     alert('Тут зовсім порожньо');
   }
 }
-  // return Promise.reject(new Error(`За запитом ${query} нічого не знайдено`));
+// return Promise.reject(new Error(`За запитом ${query} нічого не знайдено`));
